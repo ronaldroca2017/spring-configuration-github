5650de217e89d8ec43b1078772c4deb1fcce7173
ronaldroca2017 <ronald.roca.mamani>
2017-06-06 19:33:49 -0500

    commiteando base

--- src/main/java/org/configuration/spring/web/model/Product.java
+++ src/main/java/org/configuration/spring/web/model/Product.java
@@ -1,5 +1,58 @@
 package org.configuration.spring.web.model;
 
-public class Product {
+import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id_product;
+	private String name;
+	private String code;
+	private BigDecimal price;
+	private String description;
+	
+	
+	public Integer getId_product() {
+		return id_product;
+	}
+	public void setId_product(Integer id_product) {
+		this.id_product = id_product;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
 }
